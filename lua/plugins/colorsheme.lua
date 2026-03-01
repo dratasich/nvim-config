9ad3a3aed7152a11aff723cba02c98198eec4207
Denise Ratasich <dratasich@users.noreply.github.com>
2026-03-01 13:53:27 +0100

    feat: get and set solarized colorscheme

---
+++ lua/plugins/colorsheme.lua
@@ -0,0 +1,21 @@
+return {
+  "maxmx03/solarized.nvim",
+  lazy = false,
+  priority = 1000,
+  ---@type solarized.config
+  opts = {},
+  config = function(_, opts)
+    vim.o.termguicolors = true
+    vim.o.background = "dark"
+    require("solarized").setup(opts)
+    vim.cmd.colorscheme("solarized")
+  end,
+
+  -- Configure LazyVim to load solarized
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized",
+    },
+  },
+}
