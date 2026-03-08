ebf4658c9e37c40f24663a4b67bbdcbbd923cc7e
Denise Ratasich <dratasich@users.noreply.github.com>
2026-03-08 11:10:48 +0100

    feat: very important key mappings :D

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 Copied from the starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+## Setup
+
+1. Install [Neovim](https://neovim.io/doc/install/)
+2. Clone this repository and symlink it to `~/.config/nvim`
+
+## Cheat sheet
+
+- [LazyVim Keymaps](https://www.lazyvim.org/keymaps)
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- File save like with evil
+vim.keymap.set("n", "<leader>fs", ":w<CR>")
+vim.keymap.set("i", "<leader>fs", "<Esc>:w<CR>i")
+
+-- jk | Escaping!
+vim.keymap.set("i", "jk", "<ESC>")
