b91bf373b74adf53b213c6d961c06ec60e106a5f
Denise Ratasich <dratasich@users.noreply.github.com>
2026-03-17 16:04:25 +0100

    fix(lsp): common yaml formatting

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,32 @@
 return {
-  "linux-cultist/venv-selector.nvim",
-  dependencies = {
-    { "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    ft = "python", -- Load when opening Python files
+    keys = { { ",v", "<cmd>VenvSelect<cr>" } }, -- Open picker on keymap
+    opts = {
+      options = {}, -- plugin-wide options
+      search = {}, -- custom search definitions
+    },
   },
-  ft = "python", -- Load when opening Python files
-  keys = { { ",v", "<cmd>VenvSelect<cr>" } }, -- Open picker on keymap
-  opts = {
-    options = {}, -- plugin-wide options
-    search = {}, -- custom search definitions
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            yaml = {
+              format = {
+                enable = true,
+                singleQuote = true,
+                proseWrap = "preserve",
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
