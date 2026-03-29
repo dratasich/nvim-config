226dc9955cc7c32ad920d029177872dd11dba173
Denise Ratasich <dratasich@users.noreply.github.com>
2026-03-29 20:12:28 +0200

    fix(undercurl): undercurl in wezterm

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ Refer to the [documentation](https://lazyvim.github.io/installation) to get star
 ### Tweaks
 
 - I had to enable undercurl in my terminal to avoid strikethrough
-  following this guide:
+  following this guide for the standard terminals:
   [blog](https://dev.to/anurag_pramanik/how-to-enable-undercurl-in-neovim-terminal-and-tmux-setup-guide-2ld7) from [reddit post](https://www.reddit.com/r/neovim/comments/1i11z63/learn_how_to_enable_undercurl_in_neovim_for/)
+  or for wezterm see [undercurl FAQ](https://wezterm.org/faq.html#how-do-i-enable-undercurl-curly-underlines)
 
 ## Cheat sheet
 
--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Ensure undercurl is applied after colorscheme loads
+-- colors from palette in https://github.com/maxmx03/solarized.nvim/blob/main/lua/solarized/palette/solarized-light.lua
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd.highlight("DiagnosticUnderlineError guisp=#DC322F gui=undercurl")
+    vim.cmd.highlight("DiagnosticUnderlineWarn guisp=#B58900 gui=undercurl")
+    vim.cmd.highlight("DiagnosticUnderlineInfo guisp=#268BD2 gui=undercurl")
+    vim.cmd.highlight("DiagnosticUnderlineHint guisp=#268BD2 gui=undercurl")
+    vim.cmd.highlight("SpellBad guisp=#B58900 gui=undercurl")
+    vim.cmd.highlight("SpellCap guisp=#268BD2 gui=undercurl")
+    vim.cmd.highlight("SpellLocal guisp=#B58900 gui=undercurl")
+    vim.cmd.highlight("SpellRare guisp=#2AA198 gui=undercurl")
+  end,
+})
